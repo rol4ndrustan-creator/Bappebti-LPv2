5d692308fbb95240e0b119a4bf82d726e265843c
rol4ndrustan-creator <rol4nd.rustan@gmail.com>
2026-06-18 18:32:45 +0800

    Add GitHub Pages static export config to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    basePath: "/Bappebti-LPv2",
+    assetPrefix: "/Bappebti-LPv2",
+    images: {
+          unoptimized: true,
+    },
 };
 
 export default nextConfig;
